3e5fdbd1a289f5498e5f5f2031ccb20145447123
Mira Yang <miracolly330@gmail.com>
2024-06-21 12:46:16 -0400

    make toDictionary extension public

--- Sources/NetSwiftly/Encoding.swift
+++ Sources/NetSwiftly/Encoding.swift
@@ -19,7 +19,7 @@ public struct JSONBodyEncoder: BodyEncoder {
     }
 }
 
-private extension Encodable {
+public extension Encodable {
     func toDictionary() throws -> [String: Any]? {
         let data = try JSONEncoder().encode(self)
         let jsonData = try JSONSerialization.jsonObject(with: data)
